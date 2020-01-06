657c7114b3917384fe3f4210b91c711a25964d68
THE-VR7 <vineetrathor57@gmail.com>
2020-01-07 01:48:30 +0530

    Gitacora saved 3 changed files

--- index.js
+++ index.js
@@ -22,29 +22,14 @@ function auth(req,res,next) {
 console.log(req.session);
   if(!req.session.user)
   {
-  console.log(req.headers);
-  var authheader = req.headers.authorization;
-  if(!authheader) {
     var err = new Error('You are not authenticated');
     res.setHeader('WWW-Authenticate','Basic');
     err.status = 401;
     return next(err);
-  }
-var auth = new Buffer.from(authheader.split(' ')[1],'base64').toString().split(':');
-var user = auth[0];
-var pass = auth[1];
-if (user == 'admin' && pass == 'password') {
-  req.session.user = 'admin';
-  next(); // authorized
-} else {
-  var err = new Error('You are not authenticated!');
-  res.setHeader('WWW-Authenticate', 'Basic');      
-  err.status = 401;
-  next(err);
-}
+ 
   }
 else {
-  if (req.session.user === 'admin') {
+  if (req.session.user === 'authenticated') {
     console.log('req.session: ',req.session);
     next();
 }
