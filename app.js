b6ce3c2d8789ce1893570b6f2ddc024da6d8bf14
THE-VR7 <vineetrathor57@gmail.com>
2020-01-07 23:29:18 +0530

    Gitacora saved 2 changed files

--- app.js
+++ app.js
@@ -75,3 +75,13 @@ app.use((req, res, next) => {
 http.createServer(app).listen(port,hostname, () =>{
     console.log('Server running at http://%s:%s',hostname,port);
 });
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
