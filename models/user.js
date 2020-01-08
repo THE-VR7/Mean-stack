b8374be782e35632e3ee1b13c95f68797dbe725c
THE-VR7 <vineetrathor57@gmail.com>
2020-01-08 22:18:06 +0530

    Gitacora saved 3 changed files

--- models/user.js
+++ models/user.js
@@ -1,19 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportlocalmongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+User.plugin(passportlocalmongoose);
 module.exports = mongoose.model('User',User);
