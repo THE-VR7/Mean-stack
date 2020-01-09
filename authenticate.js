d08b6a161b86d260db23adeeca1d4807c89e09d7
THE-VR7 <vineetrathor57@gmail.com>
2020-01-10 01:23:39 +0530

    Gitacora saved 2 changed files

--- authenticate.js
+++ authenticate.js
@@ -21,19 +21,18 @@ var opts = {};
 opts.jwtFromrequrest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtPassport = passport.use(new Jwtstrategy(opts, 
+exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
-        console.log("JWT_PAYLOAd",jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err,user) => {
-            if(err)
-            {
-                return done(err,false);
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
             }
-            else if(user){
-                return done(null,user);
+            else if (user) {
+                return done(null, user);
             }
             else {
-                return done(null,false);
+                return done(null, false);
             }
         });
     }));
