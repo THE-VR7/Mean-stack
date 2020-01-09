03269be602b25afb8a97f185fa34a38eeead478e
THE-VR7 <vineetrathor57@gmail.com>
2020-01-09 23:49:20 +0530

    Gitacora saved 1 changed files

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,41 @@
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
+var Jwtstrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken');
+
+var config = require('./config');
+
 
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = (user) => {
+    return jwt.sign(user,config.secretKey
+        , {expiresIn:3600});
+};
+
+var opts = {};
+opts.jwtFromrequrest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(new Jwtstrategy(opts, 
+    (jwt_payload, done) => {
+        console.log("JWT_PAYLOAd",jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err,user) => {
+            if(err)
+            {
+                return done(err,false);
+            }
+            else if(user){
+                return done(null,user);
+            }
+            else {
+                return done(null,false);
+            }
+        });
+    }));
+
+exports.verifyUser = passport.authenticate('jwt',{session: false});
